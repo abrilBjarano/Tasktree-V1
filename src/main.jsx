b7eb1b84c0756cb4cab03096ca0a9e34c3a72f4f
abrilxcx <abrlbjrn@gmail.com>
2024-07-16 12:11:49 -0600

    Implemented Tasktree to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+
+import { Tasktree } from './Tasktree';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Tasktree />
   </React.StrictMode>,
 )
