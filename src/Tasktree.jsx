c98c67f45841add9d82b0577992a4169249323d9
abrilxcx <abrlbjrn@gmail.com>
2024-07-17 14:08:50 -0600

    🧑‍🎨 Made important cosmetic change for views in large and small displays

--- src/Tasktree.jsx
+++ src/Tasktree.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "./components/Navbar";
 import { TaskProvider } from "./context/TaskProvider";
 import { TaskRoutes } from "./routes/TaskRoutes";
+import './styles.css';
 
 export const Tasktree = () => {
 
@@ -9,8 +10,14 @@ export const Tasktree = () => {
 
    return (
       <TaskProvider>
+
          <Navbar/>
-         <TaskRoutes/>
+
+
+         <div className="mx-5 mx-lg-15">
+            <TaskRoutes/>
+         </div>
+
       </TaskProvider>
    )
 }
