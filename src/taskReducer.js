2de1494eea16537a28fb92f14b3165ce06d4809e
abrilxcx <abrlbjrn@gmail.com>
2024-07-22 13:10:56 -0600

    Upgraded Add task Reducer

--- src/taskReducer.js
+++ src/taskReducer.js
@@ -2,22 +2,7 @@ export const taskReducer = ( state, action ) => {
    
    switch( action.type ){
       case 'Add task':
-         return {
-            ...state,
-            incomplete: [ ...state.incomplete, action.payload ]
-         }
-      
-      case 'Delete task':
-         return {
-            ...state,
-            incomplete: state.incomplete.filter( task => task.id !== action.payload )
-         }
-      
-      case 'Delete completed':
-         return {
-            ...state,
-            completed: []
-         }
+         return [ ...state, action.payload ];
       
       default:
          return state;
